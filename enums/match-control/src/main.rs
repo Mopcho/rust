5edfeb439c3c3aeffaeb3fe12501f50a5a73dcfd
Mopcho <47301161+Mopcho@users.noreply.github.com>
2023-11-23 13:23:37 +0200

    coin app using match and enums

--- enums/match-control/src/main.rs
+++ enums/match-control/src/main.rs
@@ -1,3 +1,32 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("{:?}", state);
+            25
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let coin1 = Coin::Quarter(UsState::Alaska);
+
+    let answer = value_in_cents(coin1);
+
+    println!("{answer}");
 }
